160d017cb5365bdb3600dd5d3a79dd0627bce5c6
Muhammad-Taimur <muhammadtaimurhassan@gmail.com>
2020-07-13 14:16:34 +0100

    Adding Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './Components/signup/signup.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo : '/login', pathMatch: 'full'},
+  {path:'signup', component: SignupComponent},
+  {path:'login', component:LoginComponent},
+  {path:'**', component:NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
