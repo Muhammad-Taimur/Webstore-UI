8d0b77650a0493984b6dd8b51dfbe2efa719ba41
Muhammad-Taimur <muhammadtaimurhassan@gmail.com>
2020-07-27 20:21:38 +0100

    Getting image from Image api with ID

--- src/app/Components/productdetail/productdetail.component.ts
+++ src/app/Components/productdetail/productdetail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
 import {NgxGalleryImage} from '@kolkov/ngx-gallery';
 import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
+import { ProductService } from 'src/app/Services/product.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 
 @Component({
@@ -11,12 +15,51 @@ import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 })
 export class ProductdetailComponent implements OnInit {
   
-  
+  public productservice = []
+  public errMsg;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
-  constructor() { }
+  public name;
+  public productBlob= [];
+  public imagePath;
 
+  constructor(private _productservice:ProductService,
+    private router: Router,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService) { }
+
+    //test
+    
   ngOnInit(): void {
+
+ this._productservice.getProduct()
+        .subscribe((data) => {
+          this.productservice = data.body;
+          // this.imageBlob = data.body
+          let json = JSON.parse(JSON.stringify(data.body))
+         console.log(json)
+        for (var i =0; i < json.length; i++){
+        console.log(data.body[i].productBlob[i])
+         this.galleryImages = [
+          {
+            small: 'assets/1.jpg',
+            medium: 'assets/1.jpg',
+            big: 'assets/1.jpg'
+          },
+          {
+            small: 'assets/2.jpg',
+            medium: 'assets/2.jpg',
+            big: 'assets/3.jpg'
+          }
+        ];
+      }
+        },
+        error => {this.errMsg = error,
+          this.spinner.hide()
+        console.log(error)});
+
+
+
 
 
     this.galleryOptions = [
@@ -24,7 +67,9 @@ export class ProductdetailComponent implements OnInit {
         width: '600px',
         height: '400px',
         thumbnailsColumns: 4,
-        imageAnimation: NgxGalleryAnimation.Slide
+        imageAnimation: NgxGalleryAnimation.Slide,
+        arrowPrevIcon: 'fa fa-chevron-left',
+        arrowNextIcon: 'fa fa-chevron-right',
       },
       // max-width 800
       {
